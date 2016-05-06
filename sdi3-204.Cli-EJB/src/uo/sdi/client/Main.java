2c98c5e5aa05a04d0bfdb7676d638dd5afdfaf30
DavidSFdez <davidsfdez@gmail.com>
2016-05-06 09:29:16 +0200

    subete zorra

--- sdi3-204.Cli-EJB/src/uo/sdi/client/Main.java
+++ sdi3-204.Cli-EJB/src/uo/sdi/client/Main.java
@@ -1,54 +1,129 @@
 package uo.sdi.client;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.LinkedList;
 import java.util.List;
 
+import uo.sdi.business.RattingsService;
 import uo.sdi.business.TripsService;
 import uo.sdi.business.UsersService;
+import uo.sdi.business.exception.EntityNotFoundException;
 import uo.sdi.business.impl.RemoteEJBServiceLocator;
+import uo.sdi.model.Rating;
 import uo.sdi.model.Trip;
 import uo.sdi.model.User;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// TODO Auto-generated method stub
+    public static void main(String[] args) throws Exception {
+	run();
 
     }
 
-    private void run() throws Exception {
-	User user;
+    private static void run() throws Exception {
+
+	listarUsuariosSistema();
+
+	deshabilitarUsuario();
+
+	listarComentariosYPuntuaciones();
 	
+	eliminarRatting();
+    }
+
+
+    private static void listarUsuariosSistema() throws Exception {
+	User user;
+
 	UsersService us = new RemoteEJBServiceLocator().createUsersService();
 	user = us.findById(101L);
-	
+
 	TripsService ts = new RemoteEJBServiceLocator().createTripsService();
+
+	List<Trip> promotedTrips = ts.findAllPromoted(user.getId());
+	List<Trip> participateTrips = ts.findAllParticipated(user.getId());
+
+	imprimirDatosUsuarioViajes(user, promotedTrips.size(),
+		participateTrips.size());
+
+    }
+
+    private static void imprimirDatosUsuarioViajes(User user,
+	    int numberPromoted, int numberParticipated) {
+	System.out.println("\nId: " + user.getId() + "\nUsuario: "
+		+ user.getLogin());
+
+	System.out.println("-----------------------------");
+	System.out.println("\nPromocionados: " + numberPromoted
+		+ "\nParticipa: " + numberParticipated);
+	System.out.println("-----------------------------");
+
+    }
+
+    private static void deshabilitarUsuario() throws Exception {
+
+	User user;
+
+	UsersService us = new RemoteEJBServiceLocator().createUsersService();
+	user = us.findById(101L);
+
+	if (user != null)
+	    us.cancelUser(user.getId());
+    }
+
+    private static void listarComentariosYPuntuaciones() {
 	
-	List<Trip> trips = ts.listRelated(user.getId());
+	RattingsService rs = new RemoteEJBServiceLocator().createRattingsService();
 	
-	imprimirDatos(user,trips);
-	
-	
-	
-	
-	/*AlumnosService service = new RemoteEjbServicesLocator().getAlumnosService();
-	List<Alumno> alumnos = service.getAlumnos();
-	printHeader();
-	for (Alumno a : alumnos) {
-	printLine(a);
-	}*/
+	try {
+	    
+		List<Trip> trips = new RemoteEJBServiceLocator()
+			.createTripsService().findAll();
+
+		List<Trip> tripsLastMonth = new LinkedList<>();
+		Date actual = new Date();
+		Date unMesAntes = getNewDateMonth(actual,-1);
+		
+		for(Trip t : trips){
+		    if(t.getArrivalDate().after(unMesAntes) && t.getArrivalDate().before(actual)){
+			tripsLastMonth.add(t);
+		    }
+		}
+
+		List<Rating> rattings = new LinkedList<>();
+		for(Trip t : tripsLastMonth){
+		    rattings.add(rs.listByTrip(t.getId()));
+		}
+		
+		imprimeRattings(rattings);
+	    
+	} catch (Exception e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
 	}
 
-    private void imprimirDatos(User user, List<Trip> trips) {
-	System.out.println("\nUsuario: "+user.getLogin()+"\nId: "+user.getId());
-	int promocionados=0;
-	int participa=0;
-	for(Trip t : trips){
-	    if(t.getPromoterId().equals(user.getId()))
-		promocionados++;
-	    else
-		participa++;
-	}
-	    System.out.println("\nPromocionados: "+promocionados+"\nParticipa: "+participa);
+    }
+
+    private static Date getNewDateMonth(Date actual, int months) {
+	    Calendar cal = Calendar.getInstance();
+	    cal.setTime(actual);
+	    cal.add(Calendar.MONTH, months); 
+	    return cal.getTime();
+    }
+
+    private static void imprimeRattings(List<Rating> rattings) {
+	// TODO Auto-generated method stub
 	
     }
+    
+    private static void eliminarRatting() {
+	RattingsService rs = new RemoteEJBServiceLocator().createRattingsService();
+	
+	Rating r = new Rating();
+	
+	rs.delete(r.getId());
+	
+    }
+
 }
